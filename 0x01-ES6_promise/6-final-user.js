51041a119c03758d46f5d0fdc1916970f6c0e8a1
mahlet <akotet2123@gmail.com>
2023-07-26 01:14:41 +0000

    bug fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,8 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => ({
-      status: results.map((result) => result.status),
-      value: results.map((result) => (result.status === 'fulfilled' ? result.value : String(result.reason))),
-    }));
+    .then((results) => (
+      results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : String(result.reason),
+      }))
+    ));
 }
