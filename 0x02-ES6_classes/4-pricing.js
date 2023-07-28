462816da4f24885ba004346e78d62de67b6fd346
mahlet <akotet2123@gmail.com>
2023-07-28 13:03:34 +0000

    eslint fix

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,21 +1,21 @@
-import Currency from "./3-currency";
+import Currency from './3-currency';
 
 export default class Pricing {
-  constructor(amount=0, currency=null) {
+  constructor(amount = 0, currency = null) {
     this._amount = amount;
     this._currency = currency;
   }
 
   set amount(value) {
     if (typeof value !== 'number') {
-      throw new TypeError('Amount must be a number')
+      throw new TypeError('Amount must be a number');
     }
     this._amount = value;
   }
 
   set currency(value) {
     if (!(value instanceof Currency)) {
-      throw new TypeError('currency must be a Currency')
+      throw new TypeError('currency must be a Currency');
     }
     this._currency = value;
   }
@@ -29,10 +29,10 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency._name} ${this._currency._code}`
+    return `${this._amount} ${this._currency._name} ${this._currency._code}`;
   }
 
-  static convertPrice(amount=0, conversionRate=0) {
+  static convertPrice(amount = 0, conversionRate = 0) {
     if (typeof amount !== 'number') {
       throw new TypeError('amount must be a string');
     }
