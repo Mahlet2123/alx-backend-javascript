afa0b07e77cff78efbfab8b56d3516f55191791a
mahlet <akotet2123@gmail.com>
2023-07-28 15:10:51 +0000

    bug fix

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -6,27 +6,13 @@ export default class Pricing {
     this.currency = currency;
   }
 
-  get amount() {
-    return this._amount;
-  }
-
   set amount(value) {
     if (typeof value !== 'number') {
-      throw new TypeError('amount must be a number');
+      throw new TypeError('Amount must be a number');
     }
     this._amount = value;
   }
 
-  /**
-   * @returns {Currency}
-   */
-  get currency() {
-    return this._currency;
-  }
-
-  /**
-   * @param {Currency} value
-   */
   set currency(value) {
     if (!(value instanceof Currency)) {
       throw new TypeError('currency must be a Currency');
@@ -34,17 +20,27 @@ export default class Pricing {
     this._currency = value;
   }
 
-  displayFullPrice() {
-    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  get amount() {
+    return this._amount;
   }
 
-  static convertPrice(amount, conversionRate) {
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency._name} ${this.currency._code}`;
+  }
+
+  static convertPrice(amount = 0, conversionRate = 0) {
     if (typeof amount !== 'number') {
       throw new TypeError('amount must be a number');
     }
+
     if (typeof conversionRate !== 'number') {
       throw new TypeError('conversionRate must be a number');
     }
-    return amount * conversionRate;
+
+    return (amount * conversionRate);
   }
 }
