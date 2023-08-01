2cc6ebd28212fdda9f17619914a6702119ff5a67
mahlet <akotet2123@gmail.com>
2023-08-01 16:14:52 +0000

    bug fixed

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,7 +2,12 @@ export default function getStudentIdsSum(studentsArray) {
   if (!Array.isArray(studentsArray)) {
     return [];
   }
-  return(
-    studentsArray.reduce((prevStudent, currStudent) => prevStudent.id || prevStudent + currStudent.id, 0)
+  return (
+    studentsArray.reduce(
+      (
+        prevStudent, currStudent,
+      ) => prevStudent.id || prevStudent + currStudent.id,
+      0,
+    )
   );
 }
