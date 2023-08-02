25613b6c4e3f97fb0cc44c3dbc2655a9e7a0105b
mahlet <akotet2123@gmail.com>
2023-08-02 11:13:54 +0000

    0. Creating an interface for a student

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -21,3 +21,26 @@ const student2: Student = {
 
 const studentsList = Array<Student>;
 studentsList = [student1, student2];
+
+// Function to render the table dynamically
+function renderTable() {
+  const tableBody = document.querySelector('tbody');
+
+  studentsList.forEach(student => {
+    const row = document.createElement('tr');
+    const firstNameCell = document.createElement('td');
+    const locationCell = document.createElement('td');
+
+    firstNameCell.textContent = student.firstName;
+    locationCell.textContent = student.location;
+
+    row.appendChild(firstNameCell);
+    row.appendChild(locationCell);
+
+    tableBody.appendChild(row);
+  });
+}
+
+// Call the renderTable function to display the table on page load
+window.onload = renderTable;
+
